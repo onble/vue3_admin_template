91f41018a398b34f0e292b2ec83e008dbb4d4647
“onble” <62949816+onble@users.noreply.github.com>
2024-04-04 17:49:25 +0800

    P33

--- src/main.ts
+++ src/main.ts
@@ -6,39 +6,29 @@ import 'element-plus/dist/index.css'
 // 配置element-plus国际化 下面的注释帮助ts进行忽略报错
 //@ts-ignore
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+// 引入模板的全局的样式
+import '@/styles/index.scss'
+// svg插件需要配置代码
+import 'virtual:svg-icons-register'
+// 引入自定义插件对象：注册整个项目全局组件
+import gloalComponent from '@/components'
+// 引入路由
+import router from './router'
+// 引入仓库
+import pinia from './store'
+
 // 获取应用实例对象
 const app = createApp(App)
+
 // 安装element-plus插件
 app.use(ElementPlus, {
     locale: zhCn, // elemet-plus国际化配置
 })
-// console.log(import.meta.env)
-// svg插件需要配置代码
-import 'virtual:svg-icons-register'
-
-// import SvgIcon from '@/components/SvgIcon/index.vue'
-// app.component('SvgIcon',SvgIcon);
-
-// 引入自定义插件对象：注册整个项目全局组件
-import gloalComponent from '@/components'
-// console.log(gloalComponent)
 // 安装自定义插件
 app.use(gloalComponent)
-
-// 引入模板的全局的样式
-import '@/styles/index.scss'
+// 安装仓库
+app.use(pinia)
+// 注册模板路由
+app.use(router)
 // 将应用挂在到挂载点上
-
-/* // 测试代码：测试假的接口能否使用
-import axios from 'axios'
-// 登录接口
-axios({
-    url: '/api/user/login',
-    method: 'post',
-    data: {
-        username: 'admin',
-        password: '1121111',
-    },
-}) */
-
 app.mount('#app')
---
+++ src/views/login/index.vue
@@ -0,0 +1,117 @@
+<template>
+    <div>
+        <div class="login_container">
+            <el-row>
+                <el-col :span="12" :xs="0"></el-col>
+                <el-col :span="12" :xs="24">
+                    <el-form class="login_form">
+                        <h1>Hello</h1>
+                        <h2>欢迎来到硅谷甄选</h2>
+                        <el-form-item>
+                            <el-input
+                                :prefix-icon="User"
+                                v-model="loginForm.username"
+                            ></el-input>
+                        </el-form-item>
+                        <el-form-item>
+                            <el-input
+                                :prefix-icon="Lock"
+                                type="password"
+                                v-model="loginForm.password"
+                                show-password
+                            ></el-input>
+                        </el-form-item>
+                        <el-form-item>
+                            <el-button
+                                class="login_btn"
+                                type="primary"
+                                size="default"
+                                @click="login"
+                                :loading="loading"
+                            >
+                                登录
+                            </el-button>
+                        </el-form-item>
+                    </el-form>
+                </el-col>
+            </el-row>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { User, Lock } from '@element-plus/icons-vue'
+import { reactive, ref } from 'vue'
+// 引入用户相关的小仓库
+import useUserStore from '@/store/modules/user'
+import { useRouter } from 'vue-router'
+import { ElNotification } from 'element-plus'
+
+// 获取路由器
+let $router = useRouter()
+let useStore = useUserStore()
+// 收集账号与密码的数据
+let loginForm = reactive({ username: 'admin', password: '111111' })
+// 定义变量控制按钮加载效果
+let loading = ref(false)
+
+// 登录按钮回调
+const login = async () => {
+    // 加载效果：开始加载
+    loading.value = true
+    // 点击登录按钮以后干什么？
+    // 通知仓库发登录请求
+    // 请求成功->首页展示数据的地方
+    // 请求失败->弹出登录失败信息
+    try {
+        // 可以书写.then语法
+        // 保证登录成功
+        await useStore.userLogin(loginForm)
+        // 编程时导航跳转到展示数据首页
+        $router.push('/')
+        // 登录成功提示信息
+        ElNotification({
+            type: 'success',
+            message: '登录成功',
+        })
+    } catch (error) {
+        // 登录失败的提示信息
+        ElNotification({
+            type: 'error',
+            message: (error as Error).message,
+        })
+    } finally {
+        loading = false
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.login_container {
+    width: 100%;
+    height: 100vh;
+    background: url('@/assets/images/background.jpg') no-repeat;
+    background-size: cover;
+
+    .login_form {
+        position: relative;
+        width: 80%;
+        top: 30vh;
+        background: url('@/assets/images/login_form.png') no-repeat;
+        background-size: cover;
+        padding: 40px;
+        h1 {
+            color: white;
+            font-size: 40px;
+        }
+        h2 {
+            font-size: 20px;
+            color: white;
+            margin: 20px 0px;
+        }
+        .login_btn {
+            width: 100%;
+        }
+    }
+}
+</style>
