13b91450f4bb5e8f4426ae5d474ef5a7b55db56f
“onble” <62949816+onble@users.noreply.github.com>
2024-04-14 21:39:10 +0800

    P43

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -6,37 +6,53 @@
             <!-- 展示菜单 -->
             <el-scrollbar class="scrollbar">
                 <!-- 菜单组件 -->
-                <el-menu background-color="#001529" text-color="white">
+                <el-menu
+                    :default-active="$route.path"
+                    background-color="#001529"
+                    text-color="white"
+                >
                     <Menu :menuList="userStore.menuRoutes"></Menu>
                 </el-menu>
             </el-scrollbar>
         </div>
         <!-- 顶部导航 -->
-        <div class="layout_tabbar"></div>
+        <div class="layout_tabbar">
+            <!-- layout组件的顶部导航tabbar -->
+            <Tabbar></Tabbar>
+        </div>
         <!-- 内容展示区域 -->
-        <div class="layout_main"></div>
+        <div class="layout_main">
+            <Main></Main>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+// 获取路由对象
+import { useRoute } from 'vue-router';
 // 引入左侧菜单logo子组件
 import Logo from './logo/index.vue';
 // 引入菜单组件
 import Menu from './menu/index.vue';
-
+// 右侧内容展示区域
+import Main from './main/index.vue';
+// 引入顶部tabbar组件
+import Tabbar from './tabbar/index.vue';
 // 获取用户相关的小仓库
 import useUserStore from '@/store/modules/user';
 let userStore = useUserStore();
-console.log(userStore);
+
+// 获取路由对象
+let $route = useRoute();
 </script>
 
 <style scoped lang="scss">
 .layout_container {
     width: 100%;
     height: 100vh;
-    color: white;
 
     .layout_slider {
+        color: white;
         width: $base-menu-width;
         height: 100vh;
         background: $base-menu-background;
@@ -56,7 +72,6 @@ console.log(userStore);
         position: fixed;
         width: calc(100% - $base-menu-width);
         height: $base-tabbar-height;
-        background: cyan;
         top: 0px;
         left: $base-menu-width;
     }
