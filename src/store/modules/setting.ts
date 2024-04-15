6e34c2898d5370dfb37e1f797078f79c6cc081ec
“onble” <62949816+onble@users.noreply.github.com>
2024-04-15 15:34:49 +0800

    P46

--- src/store/modules/setting.ts
+++ src/store/modules/setting.ts
@@ -1,11 +1,12 @@
 // 小仓库:layout组件相关配置仓库
 import { defineStore } from 'pinia';
-const useLayOutSettingStore = defineStore('SettingSotre', {
+const useLayoutSettingStore = defineStore('SettingSotre', {
     state: () => {
         return {
             fold: false, // 用户控制菜单折叠还是收起控制
+            refsh: false, // 仓库这个属性用于控制刷新效果
         };
     },
 });
 
-export default useLayOutSettingStore;
+export default useLayoutSettingStore;
