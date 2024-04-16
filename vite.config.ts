bae7c5c56597ef24fad67bdf93f10812d03ee08c
“onble” <62949816+onble@users.noreply.github.com>
2024-04-16 09:55:50 +0800

    P52

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 // 引入svg需要用到插件
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 // https://vitejs.dev/config/
 
 // mock插件提供方法
-import { viteMockServe } from 'vite-plugin-mock'
-export default defineConfig(({ command }) => {
+import { viteMockServe } from 'vite-plugin-mock';
+export default defineConfig(({ command, mode }) => {
+    // 获取各种环境下的对应变量
+    const env = loadEnv(mode, process.cwd());
     return {
         plugins: [
             vue(),
@@ -33,5 +35,18 @@ export default defineConfig(({ command }) => {
                 },
             },
         },
-    }
-})
+        // 代理跨域
+        server: {
+            proxy: {
+                [env.VITE_APP_BASE_API]: {
+                    // 获取数据的服务器地址设置
+                    target: env.VITE_SERVE,
+                    // 需要代理跨域
+                    changeOrigin: true,
+                    // 路径重写
+                    rewrite: (path) => path.replace(/^\/api/, ''),
+                },
+            },
+        },
+    };
+});
