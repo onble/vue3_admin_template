0dddd887412355380ec8c4fb2f84097f9dbfd00e
“onble” <62949816+onble@users.noreply.github.com>
2024-04-18 17:28:40 +0800

    P64

---
+++ src/components/Category/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <el-card>
+        <el-form :inline="true">
+            <el-form-item label="一级分类">
+                <el-select style="width: 240px">
+                    <el-option lable="北京"></el-option>
+                    <el-option lable="上海"></el-option>
+                    <el-option lable="广州"></el-option>
+                    <el-option lable="深圳"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="二级分类">
+                <el-select style="width: 240px">
+                    <el-option lable="北京"></el-option>
+                    <el-option lable="上海"></el-option>
+                    <el-option lable="广州"></el-option>
+                    <el-option lable="深圳"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="三级分类">
+                <el-select style="width: 240px">
+                    <el-option lable="北京"></el-option>
+                    <el-option lable="上海"></el-option>
+                    <el-option lable="广州"></el-option>
+                    <el-option lable="深圳"></el-option>
+                </el-select>
+            </el-form-item>
+        </el-form>
+    </el-card>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
