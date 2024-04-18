a9e9cbe32b075dda4efdd81cf3e090f6d7cf1b54
“onble” <62949816+onble@users.noreply.github.com>
2024-04-18 22:16:19 +0800

    P65

--- src/components/Category/index.vue
+++ src/components/Category/index.vue
@@ -2,11 +2,14 @@
     <el-card>
         <el-form :inline="true">
             <el-form-item label="一级分类">
-                <el-select style="width: 240px">
-                    <el-option lable="北京"></el-option>
-                    <el-option lable="上海"></el-option>
-                    <el-option lable="广州"></el-option>
-                    <el-option lable="深圳"></el-option>
+                <el-select v-model="categoryStore.c1Id" style="width: 240px">
+                    <!-- option:label即为显示文字 value属性即为select下拉菜单收集的数据 -->
+                    <el-option
+                        v-for="c1 in categoryStore.c1Arr"
+                        :key="c1.id"
+                        :label="c1.name"
+                        :value="c1.id"
+                    ></el-option>
                 </el-select>
             </el-form-item>
             <el-form-item label="二级分类">
@@ -29,6 +32,42 @@
     </el-card>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+/* // 引入分类接口方法
+import { reqC1 } from '@/api/product/attr';
+
+// 引入生命周期函数钩子
+import { onMounted, ref } from 'vue';
+// 组件挂载完毕
+onMounted(() => {
+    // 获取一级分类的数据
+    getC1();
+});
+// 存储一级分类的数据
+let c1Arr = ref<any>([]);
+let c1Id = ref<number | string>('');
+// 获取一级分类的数据方法
+const getC1 = async () => {
+    const result = await reqC1();
+    if (result.code == 200) {
+        c1Arr.value = result.data;
+    }
+}; */
+// 引入组件挂载完毕方法
+import { onMounted } from 'vue';
+// 引入分类相关的仓库
+import useCategoryStore from '@/store/modules/category';
+let categoryStore = useCategoryStore();
+// 分类全局组件挂载完毕，通知仓库发请求获取一级分类的数据
+onMounted(() => {
+    getC1();
+});
+
+// 通知仓库获取一级分类的方法
+const getC1 = () => {
+    // 通知分类仓库发请求获取一级分类的数据
+    categoryStore.getC1();
+};
+</script>
 
 <style scoped></style>
