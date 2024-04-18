8e750dddd41e807b25217e509d14333ec84026bd
“onble” <62949816+onble@users.noreply.github.com>
2024-04-18 22:29:58 +0800

    P66

--- src/api/product/attr/index.ts
+++ src/api/product/attr/index.ts
@@ -1,5 +1,6 @@
 // 这里书写属性相关的API文件
 import request from '@/utils/request';
+import type { CagegoryResponseData } from './type';
 // 属性管理模块接口地址
 enum API {
     // 获取一级分类接口地址
@@ -10,10 +11,10 @@ enum API {
     C3_URL = '/admin/product/getCategory3/',
 }
 // 获取一级分类的接口方法
-export const reqC1 = () => request.get<any, any>(API.C1_URL);
+export const reqC1 = () => request.get<any, CagegoryResponseData>(API.C1_URL);
 // 获取二级分类的接口方法
 export const reqC2 = (category1Id: number) =>
-    request.get<any, any>(API.C2_URL + category1Id);
+    request.get<any, CagegoryResponseData>(API.C2_URL + category1Id);
 // 获取三级分类的接口方法
 export const reqC3 = (category2Id: number) =>
-    request.get<any, any>(API.C3_URL + category2Id);
+    request.get<any, CagegoryResponseData>(API.C3_URL + category2Id);
--- src/store/modules/types/type.ts
+++ src/store/modules/types/type.ts
@@ -1,3 +1,4 @@
+import { CategoryObj } from '@/api/product/attr/type';
 import { RouteRecordRaw } from 'vue-router';
 // 定义小仓库数据state类型
 export interface UserState {
@@ -5,4 +6,10 @@ export interface UserState {
     menuRoutes: RouteRecordRaw[];
     username: string;
     avatar: string;
+}
+
+// 定义分类仓库state对象的ts类型
+export interface CategoryState {
+    c1Id: string | number;
+    c1Arr: CategoryObj[];
 }
