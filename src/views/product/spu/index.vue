dad198a08b868355468088022079edb709fefd3c
“onble” <62949816+onble@users.noreply.github.com>
2024-04-20 14:23:50 +0800

    P76

--- src/views/product/spu/index.vue
+++ src/views/product/spu/index.vue
@@ -1,9 +1,47 @@
 <template>
     <div>
-        <h1>SPU管理</h1>
+        <!-- 三级分类 -->
+        <Category :scene="scene"></Category>
+        <el-card style="margin: 10px 0px">
+            <el-button type="primary" size="default" icon="Plus">
+                添加SPU
+            </el-button>
+            <el-table style="margin: 10px 0px" border>
+                <el-table-column
+                    label="序号"
+                    type="index"
+                    align="center"
+                    width="80px"
+                />
+                <el-table-column label="SPU名称" />
+                <el-table-column label="SPU描述" />
+                <el-table-column label="SPU操作" />
+            </el-table>
+            <!-- 分页器 -->
+            <el-pagination
+                @size-change="sizeChange"
+                @current-change="currentChange"
+                v-model:currentPage="pageNo"
+                :page-sizes="[3, 5, 7, 9]"
+                :page-size="pageSize"
+                layout="prev, pager, next, jumper, ->, sizes, total"
+                :total="400"
+                background
+            >
+                :pager-count="7">
+            </el-pagination>
+        </el-card>
     </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+// 场景的数据
+let scene = ref<number>(0);
+// 分页器默认页码
+let pageNo = ref<number>(1);
+// 每一页展示几条数据
+let pageSize = ref<number>(3);
+</script>
 
 <style scoped></style>
