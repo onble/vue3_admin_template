ed6ae7deb6c28deb07350f0a6debdbcf1fe1d43e
“onble” <62949816+onble@users.noreply.github.com>
2024-04-20 20:45:54 +0800

    P80

--- src/api/product/spu/index.ts
+++ src/api/product/spu/index.ts
@@ -1,9 +1,23 @@
 // SPU管理模块的接口
 import request from '@/utils/request';
-import { HasSpuResponseData } from './type';
+import {
+    AllTradeMark,
+    HasSaleAttrResponseData,
+    HasSpuResponseData,
+    SaleAttrReaponseData,
+    SpuHasImg,
+} from './type';
 enum API {
     // 获取已有的SPU的数据
     HASSPU_URL = '/admin/product/',
+    // 获取全部品牌的数据
+    ALLTRADEMARK_URL = '/admin/product/baseTrademark/getTrademarkList',
+    // 获取某个SPU下的全部的售卖商品的图片数据
+    IMAGE_URL = '/admin/product/spuImageList/',
+    // 获取某一个SPU下全部的已有的销售属性接口地址
+    SPUHASSALEATTR_URL = '/admin/product/spuSaleAttrList/',
+    // 获取整个项目全部的销售属性[颜色，版本，尺码]
+    ALLSALEATTR_URL = '/admin/product/baseSaleAttrList',
 }
 // 获取某一个三级分类下已有的SPU数据
 export const reqHasSpu = (
@@ -14,3 +28,15 @@ export const reqHasSpu = (
     request.get<any, HasSpuResponseData>(
         API.HASSPU_URL + `${page}/${limmit}?category3Id=${category3Id}`,
     );
+// 获取全部的SPU的品牌的数据
+export const reqAllTradeMark = () =>
+    request.get<any, AllTradeMark>(API.ALLTRADEMARK_URL);
+// 获取某一个已有的SPu下全部商品的图片地址
+export const reqSpuImageList = (spuId: number) =>
+    request.get<any, SpuHasImg>(API.IMAGE_URL + spuId);
+// 获取某一个已有的SPU拥有多少个销售属性
+export const reqSpuHasSaleAttr = (spuId: number) =>
+    request.get<any, SaleAttrReaponseData>(API.SPUHASSALEATTR_URL + spuId);
+// 获取全部的销售属性
+export const reqAllSaleAttr = () =>
+    request.get<any, HasSaleAttrResponseData>(API.ALLSALEATTR_URL);
--- src/api/product/spu/type.ts
+++ src/api/product/spu/type.ts
@@ -27,3 +27,65 @@ export interface HasSpuResponseData extends ResponseData {
         pages: number;
     };
 }
+
+// 品牌数据的TS类型
+export interface Trademark {
+    id: number;
+    tmName: string;
+    logoUrl: string;
+}
+// 品牌接口返回的数据ts类型
+export interface AllTradeMark extends ResponseData {
+    data: Trademark[];
+}
+
+// 商品图片的ts类型
+export interface SpuImg {
+    id: number;
+    createTime: string;
+    updateTime: string;
+    spuId: number;
+    imgName: string;
+    imgUrl: string;
+}
+// 已有的SPU的照片墙数据的类型
+export interface SpuHasImg extends ResponseData {
+    data: SpuImg[];
+}
+
+// 已有的销售属性值对象ts类型
+export interface SaleAttrValue {
+    id?: number;
+    createTime: null;
+    updateTime: null;
+    spuId: number;
+    baseSaleAttrId: number;
+    saleAttrValueName: string;
+    saleAttrName: string;
+    isChecked: null;
+}
+// 存储已有的销售属性值数组类型
+export type SpuSaleAttrValueList = SaleAttrValue[];
+// 销售属性对象ts类型
+export interface SaleAttr {
+    id?: number;
+    createTime: null;
+    updateTime: null;
+    spuId: number;
+    baseSaleAttrId: number;
+    saleAttrName: string;
+    spuSaleAttrValueList: SpuSaleAttrValueList;
+}
+// SPU已有的销售属性接口返回数据ts类型
+export interface SaleAttrReaponseData extends ResponseData {
+    data: SaleAttr[];
+}
+
+// 已有的全部SPU的返回数据ts类型
+export interface HasSaleAttr {
+    id: number;
+    name: string;
+}
+export interface HasSaleAttrResponseData extends ResponseData {
+    data: HasSaleAttr[];
+}
