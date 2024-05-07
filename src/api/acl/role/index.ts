3326b29363694d6992c0e72cc5608d5aaa89cbc1
“onble” <62949816+onble@users.noreply.github.com>
2024-05-07 20:51:05 +0800

    P117

--- src/api/acl/role/index.ts
+++ src/api/acl/role/index.ts
@@ -1,13 +1,25 @@
 // 角色管理模块的接口
 import request from '@/utils/request';
-import { RoleResponseData } from './type';
+import { RoleData, RoleResponseData } from './type';
 // 枚举地址
 enum API {
     // 获取全部的职位接口
     ALLROLE_URL = '/admin/acl/role/',
+    // 新增岗位的接口地址
+    ADDROLE_URL = '/admin/acl/role/save',
+    // 跟新已有的职位
+    UPDATEROLE_URL = '/admin/acl/role/update',
 }
 // 获取全部的角色
 export const reqAllRoleList = (page: number, limit: number, roleName: string) =>
     request.get<any, RoleResponseData>(
         API.ALLROLE_URL + `${page}/${limit}/?roleName=${roleName}`,
     );
+// 添加职位与更新已有职位接口
+export const reqAddOrUpdateRole = (data: RoleData) => {
+    if (data.id) {
+        return request.put<any, any>(API.UPDATEROLE_URL, data);
+    } else {
+        return request.post<any, any>(API.ADDROLE_URL, data);
+    }
+};
--- src/views/acl/role/index.vue
+++ src/views/acl/role/index.vue
@@ -24,7 +24,7 @@
         </el-form>
     </el-card>
     <el-card style="margin: 10px 0px">
-        <el-button type="primary" size="default" icon="Plus">
+        <el-button type="primary" size="default" icon="Plus" @click="addRole">
             添加职位
         </el-button>
         <el-table border style="margin: 10px 0px" :data="allRole">
@@ -54,7 +54,12 @@
                     <el-button type="primary" size="small" icon="User">
                         分配权限
                     </el-button>
-                    <el-button type="primary" size="small" icon="Edit">
+                    <el-button
+                        type="primary"
+                        size="small"
+                        icon="Edit"
+                        @click="updateRole(row)"
+                    >
                         编辑
                     </el-button>
                     <el-button type="primary" size="small" icon="Delete">
@@ -74,15 +79,46 @@
             @size-change="sizeChange"
         />
     </el-card>
+    <!-- 添加职位与更新已有职位的结构：对话框 -->
+    <el-dialog
+        :title="RoleParams.id ? '添加职位' : '添加职位'"
+        v-model="dialogVisite"
+    >
+        <el-form :model="RoleParams" :rules="rules" ref="form">
+            <el-form-item label="职位名称" prop="roleName">
+                <el-input
+                    placeholder="请你输入职位名称"
+                    v-model="RoleParams.roleName"
+                ></el-input>
+            </el-form-item>
+        </el-form>
+        <template #footer>
+            <span>
+                <el-button
+                    type="primary"
+                    size="default"
+                    @click="dialogVisite = false"
+                >
+                    取消
+                </el-button>
+                <el-button type="primary" size="default" @click="save">
+                    确定
+                </el-button>
+            </span>
+        </template>
+    </el-dialog>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
 // 请求方法
-import { reqAllRoleList } from '@/api/acl/role';
-import type { RoleResponseData, Records } from '@/api/acl/role/type';
+import { reqAddOrUpdateRole, reqAllRoleList } from '@/api/acl/role';
+import type { RoleResponseData, Records, RoleData } from '@/api/acl/role/type';
 // 引入骨架的仓库
 import userLayOutSettingStore from '@/store/modules/setting';
+import { reactive } from 'vue';
+import { ElMessage } from 'element-plus';
+import { nextTick } from 'vue';
 const settingStore = userLayOutSettingStore();
 // 当前页码
 let pageNo = ref<number>(1);
@@ -94,6 +130,14 @@ let keyword = ref<string>('');
 let allRole = ref<Records>([]);
 // 职位总个数
 let total = ref<number>(0);
+// 控制对话框的显示与隐藏
+let dialogVisite = ref<boolean>(false);
+// 收集新增岗位数据
+let RoleParams = reactive<RoleData>({
+    roleName: '',
+});
+// 获取form组件实例
+let form = ref<any>();
 // 组件挂载完毕
 onMounted(() => {
     getHasRole();
@@ -126,6 +170,63 @@ const search = () => {
 // 重置按钮的回调
 const reset = () => {
     settingStore.refsh = !settingStore.refsh;
+};
+// 添加职位按钮的回调
+const addRole = () => {
+    // 对话框显示出来
+    dialogVisite.value = true;
+    // 清空数据
+    Object.assign(RoleParams, {
+        roleName: '',
+        id: 0,
+    });
+    // 清空上一次表单校验错误结果
+    nextTick(() => {
+        form.value.clearValidate('roleName');
+    });
+};
+// 更新已有的职位按钮的回调
+const updateRole = (row: RoleData) => {
+    // 显示出对话框
+    dialogVisite.value = true;
+    // 存储已有的职位---带有ID的
+    Object.assign(RoleParams, row);
+    // 清空上一次表单校验错误结果
+    nextTick(() => {
+        form.value.clearValidate('roleName');
+    });
+};
+// 自定义校验规则的回调
+const validatorRoleName = (rule: any, value: any, callBack: any) => {
+    if (value.trim().length >= 2) {
+        callBack();
+    } else {
+        callBack(new Error('职位名称至少两位'));
+    }
+};
+// 职位校验规则
+const rules = {
+    roleName: [
+        { required: true, trigger: 'blur', validator: validatorRoleName },
+    ],
+};
+// 确定按钮的回调
+const save = async () => {
+    // 表单校验结果，结果通过再发请求，结果没有通过不应该再发请求
+    await form.value.validate();
+    // 添加职位|更新职位的请求
+    const result: any = await reqAddOrUpdateRole(RoleParams);
+    if (result.code == 200) {
+        // 提示文字
+        ElMessage({
+            type: 'success',
+            message: RoleParams.id ? '更新成功' : '添加成功',
+        });
+        // 对话框显示
+        dialogVisite.value = false;
+        // 再次获取全部的已有的职位
+        getHasRole(RoleParams.id ? pageNo.value : 1);
+    }
 };
 </script>
 
