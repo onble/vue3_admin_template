3af17c98dd1add077d8cbc74547be3b4df210fd4
“onble” <62949816+onble@users.noreply.github.com>
2024-05-10 17:20:50 +0800

    P131

--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,9 +1,68 @@
 <template>
-    <div>
-        <h1>我是数据大屏一级路由组件</h1>
+    <div class="container">
+        <!-- 数据大屏展示内容区域 -->
+        <div class="screen" ref="screen">
+            <div class="top"><Top></Top></div>
+            <div class="bottom">
+                <div class="left"><div class="lbtn">首页</div></div>
+                <div class="center"></div>
+                <div class="right">右侧</div>
+            </div>
+        </div>
     </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+// 引入顶部的子组件
+import Top from './components/top/index.vue';
+// 获取数据大屏展示内容盒子的DOM元素
+let screen = ref();
+onMounted(() => {
+    screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`;
+});
+// 定义大屏缩放比例
+function getScale(w = 1920, h = 1080) {
+    const ww = window.innerWidth / w;
+    const wh = window.innerHeight / h;
+    return ww < wh ? ww : wh;
+}
+// 监听视口变化
+window.onresize = () => {
+    screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`;
+};
+</script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.container {
+    width: 100vw;
+    height: 100vh;
+    background: url(./images/bg.png) no-repeat;
+    background-size: cover;
+
+    .screen {
+        position: fixed;
+        width: 1920px;
+        height: 1080px;
+        left: 50%;
+        top: 50%;
+        transform-origin: left top;
+        .top {
+            width: 100%;
+            height: 40px;
+        }
+        .bottom {
+            display: flex;
+            .right {
+                flex: 1;
+            }
+            .left {
+                flex: 1;
+            }
+            .center {
+                flex: 2;
+            }
+        }
+    }
+}
+</style>
