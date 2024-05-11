dce4e9d48fb5473d9b634a84dec3322db05fe51e
“onble” <62949816+onble@users.noreply.github.com>
2024-05-11 14:22:01 +0800

    P136

--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -10,7 +10,10 @@
                     <Sex class="sex"></Sex>
                     <Age class="age"></Age>
                 </div>
-                <div class="center"></div>
+                <div class="center">
+                    <Map class="map"></Map>
+                    <Line class="line"></Line>
+                </div>
                 <div class="right">右侧</div>
             </div>
         </div>
@@ -25,6 +28,9 @@ import Top from './components/top/index.vue';
 import Tourist from './components/tourist/index.vue';
 import Sex from './components/sex/index.vue';
 import Age from './components/age/index.vue';
+// 引入中间两个子组件
+import Map from './components/map/index.vue';
+import Line from './components/line/index.vue';
 // 获取数据大屏展示内容盒子的DOM元素
 let screen = ref<HTMLElement | null>(null);
 onMounted(() => {
@@ -86,6 +92,15 @@ window.onresize = () => {
             }
             .center {
                 flex: 2;
+                display: flex;
+                flex-direction: column;
+                .map {
+                    flex: 3;
+                }
+                .line {
+                    flex: 1;
+                    background: orange;
+                }
             }
         }
     }
